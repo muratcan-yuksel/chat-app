9af6882f9b604b13d849efef05ccb310560d20a0
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-09-15 16:18:09 +0300

    now it creates a new output on ech click. so it's ok

--- src/components/Input.js
+++ src/components/Input.js
@@ -7,55 +7,40 @@ import Button from "react-bootstrap/Button";
 // import "bootstrap/dist/css/bootstrap.min.css";
 // import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 // The added element component
-const AddedElement = () => (
-  <p>
-    <input placeholder="text box" />
-  </p>
-);
-const Input = () => {
-  const [state, setState] = useState();
-  const [output, setOutput] = useState([]);
-  const [count, setCount] = useState(0); // Name it however you wish
 
+const Input = () => {
+  //get the textarea input
+  const [text, setText] = useState("");
+  const [arr, setArr] = useState([]);
+
+  //create handlechange function
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+  //create handleSubmit function
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setArr([...arr, text]);
+  };
   useEffect(() => {
     // Do something
-    console.log("effect");
-    return <p>output</p>;
-  }, [output]);
-
-  const handleClick = (e) => {
-    e.preventDefault();
-    setCount(count + 1);
-    setOutput(state, ...output);
-    console.log(output);
-  };
-
-  // const createPara = output.map((item) => {
-  //   console.log(item);
-  // });
-
-  const handleText = (e) => {
-    console.log(e.target.value);
-    setState(e.target.value);
-  };
-
-  // const mapped = output.map((item) => {});
-
+    console.log(arr);
+  }, [arr]);
+  let texty = arr.map((item) => {
+    return <p>{item}</p>;
+  });
   return (
     <div>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
           <Form.Label>Example textarea</Form.Label>
-          <Form.Control onChange={handleText} as="textarea" rows={3} />
+          <Form.Control onChange={handleChange} as="textarea" rows={3} />
         </Form.Group>
-        <Button onClick={handleClick} variant="primary" type="submit">
+        <Button variant="primary" type="submit">
           Submit
         </Button>
       </Form>
-      {/* <div> {createPara} </div> */}
-      {[...Array(count)].map((_, i) => (
-        <AddedElement key={i} />
-      ))}
+      {texty}
     </div>
   );
 };
