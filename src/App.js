6ea64a84ce745005156054c34b1cbe5ac361fdb1
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-09-17 13:27:24 +0300

    adding realtime database

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
+import { useState, useEffect } from "react";
 import Input from "./components/Input";
 import { firebase } from "./initFirebase";
-const db = firebase.firestore();
+// const db = firebase.firestore();
 function App() {
-  console.log(db.collection("texts"));
+  // const [state, setState] = useState({});
+  // // console.log(db);
+  // db.collection("cafes")
+  //   .get()
+  //   .then((snapshot) => {
+  //     snapshot.docs.forEach((doc) => {
+  //       // console.log(doc.data());
+  //       setState(doc);
+  //     });
+  //   });
+  // console.log(state.data().name);
+
+  const ref = firebase.firestore().collection("cafes");
+  const [state, setState] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  function getCafes() {
+    setLoading(true);
+    ref.onSnapshot((querySnapshot) => {
+      const items = [];
+      querySnapshot.forEach((doc) => {
+        items.push(doc.data());
+      });
+      setState(items);
+      setLoading(false);
+    });
+  }
+  useEffect(() => {
+    getCafes();
+    console.log(state);
+  }, []);
+  console.log(state[0].name);
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
   return (
     <div className="App">
       <Input />
+      {state[0].name}
     </div>
   );
 }
 
 export default App;
-
-{
-  /* <div>
-<h1>React & Firebase</h1>
-<h2>By @farazamiruddin</h2>
-<code>
-  <pre>{JSON.stringify(firebaseApp.options, null, 2)}</pre>
-</code>
-</div> */
-}
